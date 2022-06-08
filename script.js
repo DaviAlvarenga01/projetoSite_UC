8bc8f87a3267ec1d0ff22eab2dc7f5f9ee66ae3e
Davi da Silva Alvarenga <100244955+DaviAlvarenga01@users.noreply.github.com>
2022-06-08 19:30:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 
+init()
+
+function init(){
+  document.getElementById("texto").addEventListener("keyup",salvar)
+  document.getElementById("nome").addEventListener("keyup",salvar)
+}
+  
+
+function salvar(){
+ var nome = document.getElementById("nome").value
+ var texto = document.getElementById("texto").value
+ 
+ localStorage.setItem("user", nome) 
+ localStorage.setItem("content", texto) 
+}
+
+function recuperar(){
+  if("user" in localStorage){
+    document.getElementgById("nome").value = localStorage.getItem("user")
+  }
+  if("content" in localStorage){
+    document.getElementeById("texto").value = localStorage.getItem("content")
+  }
+}
+
+function limpar(){
+  localStorage.clear()
+} 
+
